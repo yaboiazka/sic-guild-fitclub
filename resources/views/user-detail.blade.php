5f87e97a939a90d6c68b7473a0c1f1665eca806b
yaboiazka <kagatazka@gmail.com>
2026-03-27 22:52:55 +0800

    add article list on user detail and articles

--- resources/views/user-detail.blade.php
+++ resources/views/user-detail.blade.php
@@ -13,7 +13,10 @@
             <h1>{{ $userDetail['phoneNumber'] }}</h1>
         </div>
     </div>
-    <div class="bg-fitclub-grey gap-6 py-10 px-20 flex items-center rounded-[40px] mx-12 my-8">
+    <div class="bg-fitclub-grey gap-6 py-10 px-20 flex flex-col rounded-[40px] mx-12 my-8">
         <h1 class="text-[36px]">Articles</h1>
+        @for ($i = 0; $i < 5; $i++)
+            <x-list-artikel-component/>
+        @endfor
     </div>
 @endsection
